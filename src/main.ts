6b3324fd2d3b3ae2345fdeb0d9fd9db1740c45c0
ItamarJanuario <juniorlemos15421@gmail.com>
2022-06-08 02:57:46 -0300

    Aula01_Ex

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+
+// main serve para deixar todo nosso arquivo principal, como por exemplo disser qual é a nossa primeira pagina que irá aparecer na tela e etc.
